40686953572a444717d13ba7174019bdb9ec6f1a
liupingting123 <1483705281@qq.com>
2017-11-14 09:46:00 +0800

    Update config.php

--- config.php
+++ config.php
@@ -18,9 +18,9 @@ $i = 0;
 * 
 * @see more details at http://rockmongo.com/wiki/configuration?lang=en_us
 */
-$MONGO["servers"][$i]["mongo_name"] = "Localhost";//mongo server name
+$MONGO["servers"][$i]["mongo_name"] = "mydb";//mongo server name
 //$MONGO["servers"][$i]["mongo_sock"] = "/var/run/mongo.sock";//mongo socket path (instead of host and port)
-$MONGO["servers"][$i]["mongo_host"] = "127.0.0.1";//mongo host
+$MONGO["servers"][$i]["mongo_host"] = "mongodb.kc-demmons.svc";//mongo host
 $MONGO["servers"][$i]["mongo_port"] = "27017";//mongo port
 $MONGO["servers"][$i]["mongo_timeout"] = 0;//mongo connection timeout
 //$MONGO["servers"][$i]["mongo_db"] = "MONGO_DATABASE";//default mongo db to connect, works only if mongo_auth=false
